cef02e324f8ee680c59c4f53cb5e60de88117bc4
praka <praka@LAPTOP-IQP3A9VQ>
2026-06-17 16:40:04 +0530

    First commit

---
+++ ssssaaaaaaaa/src/main/java/harshitha/ssssaaaaaaaa/App.java
@@ -0,0 +1,10 @@
+package harshitha.ssssaaaaaaaa;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
